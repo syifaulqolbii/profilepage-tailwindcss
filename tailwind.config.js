cf6200ab8f3b2ce8a4a19373e3180d538c26a4eb
Syifaul Qolbi <syifaul@pop-os.localdomain>
2023-02-03 23:40:08 +0700

    feat : add dark theme + minor fixing feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     container: {
       center: true,
